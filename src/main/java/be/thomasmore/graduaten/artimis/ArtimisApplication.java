4a9e5eb51aec61fececc102e04f5c033ca333f68
wesley.verbeke <wesverbeke@hotmail.com>
2020-10-24 06:40:30 +0200

    init project bericht

---
+++ src/main/java/be/thomasmore/graduaten/artimis/ArtimisApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.artimis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtimisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtimisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/graduaten/artimis/ArtimisApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.artimis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtimisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
