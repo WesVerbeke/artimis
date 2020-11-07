bd83c70bfc2b7f59a1a420beea5e5042cd795a18
wesley.verbeke <wesverbeke@hotmail.com>
2020-11-07 13:31:21 +0100

    Added BestellingProduct Entity

---
+++ src/main/java/be/thomasmore/graduaten/artimis/entity/BestellingProduct.java
@@ -0,0 +1,67 @@
+package be.thomasmore.graduaten.artimis.entity;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class BestellingProduct {
+
+    //Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bestellingProductId;
+    private Integer productId;
+    private Integer bestellingId;
+    private Integer aantal;
+    private Date einddatum;
+
+    //Many to One relations with other entities
+    @ManyToOne
+    private Product product;
+    private Bestelling bestelling;
+
+    //Constructors
+    public BestellingProduct() {
+    }
+
+    public BestellingProduct(Integer bestellingProductId, Integer productId, Integer bestellingId, Integer aantal, Date einddatum, Product product, Bestelling bestelling) {
+        this.bestellingProductId = bestellingProductId;
+        this.productId = productId;
+        this.bestellingId = bestellingId;
+        this.aantal = aantal;
+        this.einddatum = einddatum;
+        this.product = product;
+        this.bestelling = bestelling;
+    }
+
+    //Getters & Setters
+
+    public Integer getBestellingProductId() { return bestellingProductId; }
+
+    public void setBestellingProductId(Integer bestellingProductId) { this.bestellingProductId = bestellingProductId; }
+
+    public Integer getProductId() { return productId; }
+
+    public void setProductId(Integer productId) { this.productId = productId; }
+
+    public Integer getBestellingId() { return bestellingId; }
+
+    public void setBestellingId(Integer bestellingId) { this.bestellingId = bestellingId; }
+
+    public Integer getAantal() { return aantal; }
+
+    public void setAantal(Integer aantal) { this.aantal = aantal; }
+
+    public Date getEinddatum() { return einddatum; }
+
+    public void setEinddatum(Date einddatum) { this.einddatum = einddatum; }
+
+    public Product getProduct() { return product; }
+
+    public void setProduct(Product product) { this.product = product; }
+
+    public Bestelling getBestelling() { return bestelling; }
+
+    public void setBestelling(Bestelling bestelling) { this.bestelling = bestelling; }
+}
