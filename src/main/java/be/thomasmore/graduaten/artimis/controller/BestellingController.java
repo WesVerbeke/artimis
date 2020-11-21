ab536b1fc881d7f01a90803a4865fedefef4e76b
wesley.verbeke <wesverbeke@hotmail.com>
2020-11-21 14:14:22 +0100

    Implemented BestellingController and temporary data-bestelling.jsp

--- src/main/java/be/thomasmore/graduaten/artimis/controller/BestellingController.java
+++ src/main/java/be/thomasmore/graduaten/artimis/controller/BestellingController.java
@@ -1,10 +1,14 @@
 package be.thomasmore.graduaten.artimis.controller;
 
 
+import be.thomasmore.graduaten.artimis.entity.Bestelling;
 import be.thomasmore.graduaten.artimis.service.BestellingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BestellingController {
@@ -13,7 +17,11 @@ public class BestellingController {
     BestellingService bestellingService;
 
     @RequestMapping ("/data-bestelling")
-    public  String
+    public  String dataMultiple(Model model) {
+        List<Bestelling> bestellingen = bestellingService.getBestellingen();
+        model.addAttribute("bestellingen", bestellingen);
+        return "data-bestelling";
+    }
 
 
 }
