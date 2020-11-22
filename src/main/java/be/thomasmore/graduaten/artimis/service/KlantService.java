b62c672511176290af2ee59e78665c6c70191aa2
wesley.verbeke <wesverbeke@hotmail.com>
2020-11-22 11:31:25 +0100

    Implemented KlantService

--- src/main/java/be/thomasmore/graduaten/artimis/service/KlantService.java
+++ src/main/java/be/thomasmore/graduaten/artimis/service/KlantService.java
@@ -1,4 +1,11 @@
 package be.thomasmore.graduaten.artimis.service;
 
+
+import be.thomasmore.graduaten.artimis.entity.Klant;
+import java.util.List;
+
 public interface KlantService {
+    Klant getKlantById(Long klantId);
+    List<Klant> getKlanten();
+    Klant addKlant(Klant klant);
 }
