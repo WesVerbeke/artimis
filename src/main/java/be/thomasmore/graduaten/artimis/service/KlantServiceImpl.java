2bdcca2d508c123a4d24ac9c2fdc4d7813570907
wesley.verbeke <wesverbeke@hotmail.com>
2020-11-22 11:35:31 +0100

    Implemented KlantServiceImpl

--- src/main/java/be/thomasmore/graduaten/artimis/service/KlantServiceImpl.java
+++ src/main/java/be/thomasmore/graduaten/artimis/service/KlantServiceImpl.java
@@ -1,4 +1,25 @@
 package be.thomasmore.graduaten.artimis.service;
 
-public class KlantServiceImpl {
+
+import be.thomasmore.graduaten.artimis.entity.Klant;
+import be.thomasmore.graduaten.artimis.repository.KlantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class KlantServiceImpl implements KlantService{
+
+    @Autowired
+    KlantRepository klantRepository;
+
+    @Override
+    public Klant getKlantById(Long klantId) { return klantRepository.getOne(klantId); }
+
+    @Override
+    public List<Klant> getKlanten() { return klantRepository.findAll(); }
+
+    @Override
+    public Klant addKlant(Klant klant) { return klantRepository.save(klant); }
 }
