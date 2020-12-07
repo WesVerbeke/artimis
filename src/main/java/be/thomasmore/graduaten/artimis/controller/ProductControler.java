da879b47fdcd38b2b9d86aac50c55826560d4038
wesverbeke <c1017478@hik.be>
2020-12-07 21:47:55 +0100

    Added ProductControler

---
+++ src/main/java/be/thomasmore/graduaten/artimis/controller/ProductControler.java
@@ -0,0 +1,24 @@
+package be.thomasmore.graduaten.artimis.controller;
+
+
+import be.thomasmore.graduaten.artimis.entity.Product;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductControler {
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping ("/data-product")
+    public String dataMultiple(Model model) {
+        List<Product> producten = productService.getProducten();
+        model.addAttribute("producten", producten);
+        return "data-product";
+    }
+}
