5772ad94015356d643ee1de7faaa9ec3fe5c964e
r0607003Dries <driesvandoorne@hotmail.com>
2020-12-27 03:44:41 +0100

    primary key klant en categorie op auto increment gezet en juist ingesteld en geprobeerd testdata toe te voegen (test data wordt niet teruggevonden in h2-console)
    dialect van de sql bestanden die evert heeft aangemaakt veranderd naar h2 (stond nog op generic)
    
    datasource toegevoegd zoals beschreven in 5_Spring_Boot.pptx (via View/Tool Window/Database)
    type datasource is in-memory (named) -> zelfde als in de cursus, er wordt enkel met db in RAM gewerkt, test data moet dus worden toegevoegd in de sql files zelf.

--- src/main/resources/Klant.sql
+++ src/main/resources/Klant.sql
@@ -1,12 +1,16 @@
 --DROP TABLE IF EXISTS Klant;
 
 CREATE TABLE Klant(
-klantId INT PRIMARY KEY,
+klantId INT NOT NULL AUTO_INCREMENT,
 voornaam NVARCHAR(250) NOT NULL,
 achternaam NVARCHAR(250) NOT NULL,
 geboortedatum DATE NOT NULL,
 email NVARCHAR(250) NOT NULL,
 telefoon NVARCHAR(250) NULL,
 gebruikersnaam NVARCHAR(250) NOT NULL,
-wachtwoord NVARCHAR(250) NOT NULL
+wachtwoord NVARCHAR(250) NOT NULL,
+PRIMARY KEY (klantId)
 );
+
+INSERT INTO Klant (voornaam, achternaam, geboortedatum, email, telefoon, gebruikersnaam, wachtwoord)
+VALUES ('Joske','Vermeulen', PARSEDATETIME('02-09-1971','dd-MM-yyyy'),'joskevermeulen@gmail.com','000/00 00 00','JoskeVermeulen','JV');
