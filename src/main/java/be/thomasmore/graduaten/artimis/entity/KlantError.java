5903b4f3625dd2896eefea00e65f530435804848
Dries <driesvandoorne@hotmail.com>
2021-01-13 20:51:42 +0100

    klanterror overgenomen

---
+++ src/main/java/be/thomasmore/graduaten/artimis/entity/KlantError.java
@@ -0,0 +1,10 @@
+package be.thomasmore.graduaten.artimis.entity;
+
+
+public class KlantError {
+    public static final String NAME = "klant_error";
+    public boolean hasErrors;
+    public String voornaam;
+    public String achternaam;
+    public String email;
+}
