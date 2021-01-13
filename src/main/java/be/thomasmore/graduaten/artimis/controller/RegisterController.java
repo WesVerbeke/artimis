bb48d87decc09a11be69ba810b10ab5aca19534b
Dries <driesvandoorne@hotmail.com>
2021-01-13 20:48:05 +0100

    registercontroller overgenomen

---
+++ src/main/java/be/thomasmore/graduaten/artimis/controller/RegisterController.java
@@ -0,0 +1,21 @@
+package be.thomasmore.graduaten.artimis.controller;
+
+
+import be.thomasmore.graduaten.artimis.entity.Klant;
+import be.thomasmore.graduaten.artimis.entity.KlantError;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RegisterController {
+
+    @RequestMapping("/register")
+    public String navigateAddGebruiker(Model model) {
+        Klant klant = new Klant();
+        KlantError klantError = new KlantError();
+        model.addAttribute(Klant.NAME, klant);
+        model.addAttribute(KlantError.NAME, klantError);
+        return "register";
+    }
+}
